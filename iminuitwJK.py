3136868fd8eb634c4579b62208038e936617b6f8
felipeortg <felipeortegagama@gmail.com>
2022-04-14 11:42:52 -0400

    Jackfile pruner, and configuration plotter, still not sure how to show sigma levels in plot

--- iminuitwJK.py
+++ iminuitwJK.py
@@ -8,6 +8,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import csv
 # import subprocess
@@ -31,11 +32,18 @@ def read_Jack_file(filename):
         for nn, row in enumerate(data):
             # print(row)
             if nn == 0:
-                cfgs, tl, comp = [int(row[0]), int(row[1]), int(row[2])] 
-                print("There are {0} configs, with {1} timeslices each".format(cfgs,tl))
-                if comp != 0:
-                    print("Not prepared for {0}".format(comp))
-                    raise ValueError
+                try:
+                    cfgs, tl, comp = [int(row[0]), int(row[1]), int(row[2])] 
+                    print("There are {0} configs, with {1} timeslices each".format(cfgs,tl))
+                    if comp != 0:
+                        print("Not prepared for {0}".format(row))
+                        raise ValueError
+                except Exception as e:
+                    print("!!!  Error while reading header !!! \n{0}".format(row))
+                    print("Format should be: Ncfg Nt (0:r 1:c) 0 1")
+                    print("This reader only accepts real option")
+                    raise Exception
+
             else:
                 if count == 0:
                     thiscfgdata = []
@@ -48,6 +56,41 @@ def read_Jack_file(filename):
 
 
     return cfgs, tl, dataarray
+
+def prune_Jack_file(filename, pruned_cfgs):
+    """
+    Read a Jack file from Roberts format: Ncfg Nt (0:r 1:c) 0 1
+    Write it again without the pruned cfgs
+    """
+    if filename[-5:] == '.jack':
+        newfile = filename[:-5] + '_pruned.jack'
+    else:
+        newfile + '.pruned'
+
+    cfgs, tl, dataarray = read_Jack_file(filename)
+
+    newcfgs = cfgs - len(pruned_cfgs)
+
+    with open(newfile, 'w') as f:
+        # Fill this list with all our levels
+        data = csv.writer(f, delimiter=' ') #change from default comma
+
+
+        # thiscfgdata = []
+        print("Writing a real valued ensemble")
+
+        data.writerow([newcfgs, tl, 0, 0, 1])
+
+        for nn, datacfg in enumerate(dataarray):
+            np1 = nn + 1
+            if np1 in pruned_cfgs:
+                continue
+
+            for row in datacfg:
+                data.writerow(row)
+
+    print("Changed to {0} configs, with {1} timeslices each".format(newcfgs, tl))
+    return newfile
 
 
 def get_mask_from_noise(filename,nrcutoff):
@@ -388,6 +431,157 @@ def fit_data(xdata, ydata, fun, verb = 0, **kwargs):
     return mean_fit, params_up, mean_chi2
 
 
+def order_number(number, p = 1):
+    """ Get the order of a number given p significant digits """
+    # If p = 1 
+    # Assign order 0 to numbers [0.95, 9.5)
+    # Assign order 1 to numbers [9.5, 95) ...
+    # If p = 2
+    # Assign order 0 to numbers [0.995, 9.95)
+    # Assign order 1 to numbers [9.95, 99.5) ...
+    # ...
+
+    numord = int(np.floor(np.log10(number)))
+    if np.round( number *10**(-numord + p - 1)) == 10**p:
+        numord += 1
+
+    return numord
+
+def p_significant_digits(num, p=1):
+    # For 1 sd [0.95, 1.05) -> 1, [1.05, 1.15) -> 1.1
+    # For 2 sd [0.995, 1.005) -> 1, [1.005, 1.015) -> 1.01
+    # Exits numbers in [1,10) range
+
+    numord = order_number(num, p)
+
+    f = numord - (p - 1)
+
+    if p > 0:
+
+        return np.round(num/10**f) * 10 **(1 - p)
+        # Note that round chooses the closest even number 3.5 and 4.5 -> 4
+
+    else:
+        raise ValueError(f"p should be positive, not {p}")
+
+def percent_significant_digits(num, percent=10):
+    """ Write a number up to the significant digits
+    that do not change its value more than the given percent
+    return number [1,10) and exponent """
+    if np.abs(percent - 50) >=  50:
+        raise ValueError(f"percent should be in (0,100), not {percent}")
+
+    if num == 0:
+        return num, 0
+
+    change = 100
+    p = 1
+    val1 = p_significant_digits(num, p)
+    order1 = order_number(num, p)
+    
+    p += 1
+    val2 = p_significant_digits(num, p)
+    order2 = order_number(num, p)
+    # print(val1, val2)
+    change = np.abs( (val2*10**order2 - val1*10**order1)/(val1*10**order1) ) * 100
+
+    while change > percent:
+        val1 = val2
+        order1 = order2
+
+        p += 1
+        val2 = p_significant_digits(num, p)
+        order2 = order_number(num, p)
+        # print(val1, val2)
+        change = np.abs( (val2*10**order2 - val1*10**order1)/(val1*10**order1) ) * 100
+
+    digits = p - 1
+    return val1, order1, digits
+
+
+def add_fit_info_ve(p, v, e):
+    if e >= v:
+        # Only use 1 significant digit if error is bigger
+        v1sd = p_significant_digits(v, 1)
+        # Use 2 significant digits
+        e2sd = p_significant_digits(e, 2)
+
+        vord = order_number(v, 1)
+        eord = order_number(e, 2)
+
+        if 3 > vord and vord > -1 :
+
+            einvord = e2sd * 10 ** (eord)
+            v1in0 = v1sd * 10 ** (vord)
+            fit_info = f"${p} = {v1in0:.3g}({einvord:.3g})$"
+
+        elif vord > -5 :
+
+            if vord == eord:
+                # Use 1 significant digits in this case
+                e1sd = p_significant_digits(e, 1)
+                eord = order_number(e, 1)
+
+                einvord = e1sd * 10 ** (eord - vord)
+                v1in0 = v1sd * 10 ** (vord)
+                fit_info = f"${p} = {v1in0:.3g}({einvord:.3g})$"
+
+            else:
+                einvord = e2sd * 10 ** (eord - vord )
+                v1in0 = v1sd * 10 ** (vord)
+                fit_info = f"${p} = {v1in0:.3g}({einvord:.3g})$"
+
+        else:
+            einvord = e2sd * 10 ** (eord - vord)
+            fit_info = f"${p} = {v1sd:.0f}({einvord:.1f})\\times 10^{vord}$"
+
+    else:
+        # Use as many significant digits for value as error 9p precision
+        esd, eord, errd = percent_significant_digits(e, percent=9)
+
+        valpro0 = eord - (errd - 1)
+
+        vord = order_number(v, 1)
+
+        dd = 0
+
+        while vord - dd != valpro0:
+            print(valpro0, vord, dd)
+            dd += 1
+            vord = order_number(v, dd + 1)
+
+        valsd = p_significant_digits(v, dd + 1)
+
+        if 3 > vord and vord > -1 :
+
+            einvord = esd * 10 ** (eord)
+            v1in0 = valsd * 10 ** (vord)
+            fit_info = f"${p} = {v1in0:.3g}({einvord:.3g})$"
+
+        elif vord > -5 :
+
+            if vord == eord:
+                # Use 1 significant digits in this case
+                v1sd = p_significant_digits(v, 1)
+                e1sd = p_significant_digits(e, 1)
+                eord = order_number(e, 1)
+
+                einvord = e1sd * 10 ** (eord - vord)
+                v1in0 = v1sd * 10 ** (vord)
+                fit_info = f"${p} = {v1in0:.3g}({einvord:.3g})$"
+
+            else:
+                einvord = esd * 10 ** (eord - vord )
+                v1in0 = valsd * 10 ** (vord)
+                fit_info = f"${p} = {v1in0:.3g}({einvord:.3g})$"
+
+        else:
+            einvord = esd * 10 ** (eord - vord)
+            fit_info = f"${p} = {valsd:.{dd}f}({einvord:.1f})\\times 10^{vord}$"
+
+    return fit_info
+
+
 def summarize_result(mean_fit, params_ense, mean_chi2, xdata):
     mean_pars = meanense(params_ense)
     cov_pars = covmatense(params_ense,mean_pars)
@@ -397,12 +591,19 @@ def summarize_result(mean_fit, params_ense, mean_chi2, xdata):
     fit_info = [f"$\\chi^2$ / $n_\\mathrm{{dof}}$ = {mean_chi2:.1f} / ({len(xdata)} - {mean_fit.nfit}) = {mean_chi2/(len(xdata) - mean_fit.nfit):.2f}"]
 
     for p, v, e in zip(mean_fit.parameters, mean_pars, np.diag(cov_pars)**(1/2)):
+
+        #fit_info.append(add_fit_info_ve(fit_info, p, v, e))
+
         if np.round(e*1e3) == 0:
             fit_info.append(f"${p} = {v:.3f} \\pm {e:.0e}$")
         else:
             fit_info.append(f"${p} = {v:.3f} \\pm {e:.3f}$")
 
     return [mean_pars, np.diag(cov_pars)**(1/2)], cor_pars, fit_info
+
+
+        
+
 
 
 
@@ -482,6 +683,10 @@ def plot_fromfile(filename, axs, nn=0, mask = []):
         list of artists added to ax1
     """
 
+    if len(axs) == 0:
+        print("Axis list is empty, no plots")
+        return 1
+
     cfgs, npoints, xdata, ydata, xmasked, ymasked = maskdata(filename,mask )
 
     print("---")
@@ -495,46 +700,81 @@ def plot_fromfile(filename, axs, nn=0, mask = []):
 
     axs[0].set_title('Mean Data')
 
-    axs[1].plot(xdata, np.abs(e_ydata/m_ydata), 'x', c = 'C'+str(nn))
-    axs[1].axhline(0,lw=1,color='k')
-    axs[1].axhline(0.12,lw=1,color='k')
+    out = plot_data(axs[0], xdata, m_ydata, e_ydata, nn)
 
-    axs[1].set_title('err/val ratio')
+    xlims = axs[0].get_xlim()
+    ylims = axs[0].get_ylim()
 
-    cov = covmatense(ydata, m_ydata)
-    corr = cormatense(ydata, m_ydata)
+    m_ymasked = meanense(ymasked)
 
-    mat1 = axs[2].matshow(np.log(np.abs(cov)))
-    axs[2].set_title('Log |Covariance matrix|')
-    divider = make_axes_locatable(axs[2])
-    cax = divider.append_axes("right", size="5%", pad=0.1)
-    plt.colorbar(mat1, cax=cax)
+    e_ymasked = errormean(ymasked, m_ymasked)
 
-    # Assume xdata is nicely ordered
-    if len(xdata) < 6:
-        dist = 1
-    else:
-        dist = np.floor(len(xdata)/6) 
-    ticks = range(0, len(xdata), int(dist))
-    tl = [str(t+xdata[0]) for t in ticks]
+    plot_data(axs[0], xmasked, m_ymasked, e_ymasked, nn, alpha=0.5)
 
-    axs[2].set_xticks(ticks)
-    axs[2].set_xticklabels(tl)
-    axs[2].set_yticks(ticks)
-    axs[2].set_yticklabels(tl)
+    axs[0].set_xlim(xlims)
+    axs[0].set_ylim(ylims)
 
-    mat2 = axs[3].matshow(corr, vmin=0, vmax=1)
-    axs[3].set_title('Correlation matrix')
-    divider = make_axes_locatable(axs[3])
-    cax = divider.append_axes("right", size="5%", pad=0.1)
-    plt.colorbar(mat2, cax=cax)
+    if len(axs) > 1:
 
-    axs[3].set_xticks(ticks)
-    axs[3].set_xticklabels(tl)
-    axs[3].set_yticks(ticks)
-    axs[3].set_yticklabels(tl)
+        axs[1].plot(xdata, np.abs(e_ydata/m_ydata), 'x', c = 'C'+str(nn))
+        axs[1].axhline(0,lw=1,color='k')
+        axs[1].axhline(0.12,lw=1,color='k')
 
-    return plot_data(axs[0], xdata, m_ydata, e_ydata, nn)
+        xlims = axs[1].get_xlim()
+        ylims = axs[1].get_ylim()
+
+        axs[1].plot(xmasked, np.abs(e_ymasked/m_ymasked), 'x', c = 'C'+str(nn), alpha=0.5)
+
+        axs[1].set_xlim(xlims)
+        axs[1].set_ylim(ylims)
+
+        axs[1].set_title('err/val ratio')
+
+    if len(axs) > 2:
+
+        cov = covmatense(ydata, m_ydata)
+        corr = cormatense(ydata, m_ydata)
+
+        cmap = mpl.cm.RdBu_r
+        norm = mpl.colors.Normalize(vmin=-1, vmax=1)
+        mat1 = axs[2].matshow(corr, cmap = cmap, norm = norm)
+        
+        axs[2].set_title('Correlation matrix')
+        divider = make_axes_locatable(axs[2])
+        cax = divider.append_axes("right", size="5%", pad=0.1)
+        plt.colorbar(mat1, cax=cax)
+
+        # Assume xdata is nicely ordered
+        if len(xdata) < 6:
+            dist = 1
+        else:
+            dist = np.floor(len(xdata)/6) 
+        ticks = range(0, len(xdata), int(dist))
+        tl = [str(t+xdata[0]) for t in ticks]
+
+        axs[2].set_xticks(ticks)
+        axs[2].set_xticklabels(tl)
+        axs[2].set_yticks(ticks)
+        axs[2].set_yticklabels(tl)
+
+
+        if len(axs) > 3:
+
+            mat2 = axs[3].matshow(np.log(np.abs(cov)))
+            axs[3].set_title('Log |Covariance matrix|')
+            divider = make_axes_locatable(axs[3])
+            cax = divider.append_axes("right", size="5%", pad=0.1)
+            plt.colorbar(mat2, cax=cax)
+
+
+            axs[3].set_xticks(ticks)
+            axs[3].set_xticklabels(tl)
+            axs[3].set_yticks(ticks)
+            axs[3].set_yticklabels(tl)
+
+
+
+    return out
 
 
 def plot_data(ax, xd, yd, yerr, nn, **kwargs):
@@ -564,8 +804,77 @@ def plot_data(ax, xd, yd, yerr, nn, **kwargs):
         list of artists added
     """
 
+
+    # Assume the data and errors were calculated 
+
     out = ax.errorbar(xd, yd, yerr,
                  ls='', c = 'C'+str(nn),
                  marker='s',fillstyle='none',ms=4, 
                  elinewidth=1, capsize=3.5, **kwargs)
     return out
+
+def plot_cfg_fromfile(filename, axs, nn=0, mask = [], plots = ['all']):
+
+
+    cfgs, npoints, xdata, ydata, xmasked, ymasked = maskdata(filename,mask )
+
+    print("---")
+    print("xdata, shape(ydata):")
+    print(len(xdata),np.shape(ydata))
+    print("---")
+
+    if len(axs) == 0:
+        print("Axis list is empty, no plots")
+        return 1
+
+    m_ydata = meanense(ydata)
+
+    e_ydata = errormean(ydata, m_ydata)
+
+    cmap = mpl.cm.gist_rainbow
+    cmap = cmap.reversed()
+    norm = mpl.colors.Normalize(vmin=1, vmax=cfgs)
+
+
+    for nn, ycfg in enumerate(ydata):
+
+        axs[0].plot(xdata, ycfg, '_', ls='', color = cmap( norm( nn + 1 )))
+
+
+    axs[0].fill_between(xdata, m_ydata + 1*np.sqrt(cfgs) * e_ydata,
+             m_ydata - 1*np.sqrt(cfgs) * e_ydata,
+              color = 'gray',
+              alpha=1, zorder=1.7)        
+
+    axs[0].fill_between(xdata, m_ydata + 3*np.sqrt(cfgs) * e_ydata,
+             m_ydata - 3*np.sqrt(cfgs) * e_ydata,
+              color = 'gray',
+              alpha=0.3, zorder=1.6)        
+
+    axs[0].fill_between(xdata, m_ydata + 5*np.sqrt(cfgs) * e_ydata,
+             m_ydata - 5*np.sqrt(cfgs) * e_ydata,
+              color = 'gray',
+              alpha=0.1, zorder=1.5)
+
+    axs[0].fill_between(xdata, m_ydata + 7*np.sqrt(cfgs) * e_ydata,
+             m_ydata - 7*np.sqrt(cfgs) * e_ydata,
+              color = 'gray',
+              alpha=0.1, zorder=1.4)
+
+    axs[0].fill_between(xdata, m_ydata + 9*np.sqrt(cfgs) * e_ydata,
+             m_ydata - 9*np.sqrt(cfgs) * e_ydata,
+              color = 'gray',
+              alpha=0.1, zorder=1.3)
+
+    divider = make_axes_locatable(axs[0])
+    cax = divider.append_axes("bottom", size="3%", pad=0.3)
+
+    plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                 cax=cax, label='Configuration', orientation = 'horizontal')
+
+    # cax = divider.append_axes("bottom", size="3%", pad=0.3)
+
+    # plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+    #              cax=cax, label='Configuration', orientation = 'horizontal')
+
+    return axs[0]
