2126a2d73340376f6cca8ff58bae5b695bba75ed
felipeortg <felipeortegagama@gmail.com>
2022-06-22 11:18:51 -0400

    Scripts to get effmass and plot an ensemble without the need to calculate the mean by hand

---
+++ eff_mass_calc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Calculate the effective mass of correlation
+# Use ensbc for jackknife statistics
+
+# Check for ensbc
+if ! command -v ensbc &> /dev/null; then
+    echo "ensbc not installed in path..."
+    exit 1
+fi
+
+
+# Check the number of arguments
+if [[ ($# -lt 2) || ($# -gt 3) ]]; then
+    echo "Usage is: $0 effmass.jack corr.jack [dt=3]"
+    exit 1
+fi
+
+effmass=$1
+correl=$2
+
+if [[ $# -gt 2 ]]; then
+    dt=$3
+else
+    dt=3
+fi
+
+
+timetemp=`head -1 $correl | awk '{print $2}'`
+tmax=`expr $timetemp - 1 `
+tmaxdt=`expr $tmax - $dt `
+
+
+#echo "$1 = - log( real( extract( $correl , $dt, $tmax ) ) / real( extract( $correl , 0, $tmaxdt ) ) ) / $dt "
+
+ensbc "$1 = - log( real( extract( $correl , $dt + 1 , $tmax ) ) / real( extract( $correl , 1 , $tmaxdt ) ) ) / $dt "
+
+
---
+++ jackplotter.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Plot an ensemble jackknife file with errorbars
+# Use calc or calcbc and dfplotter
+# Check for dfplotter
+if ! command -v dfplotter.py &> /dev/null; then
+    echo "dfplotter.py not installed in path..."
+    exit 1
+fi
+
+# Check for calc
+if ! command -v calc &> /dev/null; then
+    echo "calc not installed in path..."
+    exit 1
+fi
+
+# Check for calcbc
+if ! command -v calcbc &> /dev/null; then
+    echo "calcbc not installed in path..."
+    exit 1
+fi
+
+# Check the number of arguments
+if [[ ($# -lt 1) || ($# -gt 2) ]]; then
+    echo "Usage is: $0 corr.jack [imag_part]"
+    exit 1
+fi
+
+correl=$1
+
+if [[ $# -gt 1 ]]; then
+    realpart=1
+else
+    realpart=0
+fi
+
+comple=`head -1 $correl | awk '{print $3}'`
+
+if [[ ( $comple -eq 0 ) && ($realpart -eq 0) ]]; then
+    calc $correl | awk '{print $1,$2,$3}' > /tmp/mean.jack
+elif [[ ($comple -eq 1 ) && ($realpart -eq 0) ]]; then
+    calcbc "real( $correl )" | awk '{print $1,$2,$3}' > /tmp/mean.jack
+elif [[ ($comple -eq 1 ) && ($realpart -eq 1) ]]; then
+    calcbc "imag( $correl )" | awk '{print $1,$2,$3}' > /tmp/mean.jack
+else
+    echo "Cannot plot the imaginary part of a real correlator ..."
+    exit 1
+fi
+
+dfplotter.py /tmp/mean.jack 0 1 2
+
+rm /tmp/mean.jack
